28aab398f5d9e7d644e9f41afe2e852adac6cc5d
BARTEK9046 <bartek9046b.w@gmail.com>
2023-03-09 14:00:00 +0100

    gotowe

--- src/Main.java
+++ src/Main.java
@@ -14,19 +14,15 @@ public class Main {
         ArrayList<Figura> figury = new ArrayList<>();
 
         System.out.println("Podaj promien kola");
-        double promien = scanner.nextDouble();
-        figury.add(new Kolo(promien));
+        figury.add(new Kolo(scanner.nextDouble()));
 
         System.out.println("Podaj dlugosc boku a trojkta:");
         System.out.println("podaj dlugosc boku b trojkata");
-        double a = scanner.nextDouble();
-        double b = scanner.nextDouble();
-        figury.add(new Trojkat(a,b));
+        figury.add(new Trojkat(scanner.nextDouble(),scanner.nextDouble()));
 
         System.out.println("Podaj dlugosc boku a prostokata:");
         System.out.println("podaj dlugosc boku b prostokata");
-        double bt = scanner.nextDouble();
-        figury.add(new Prostokat(scanner.nextDouble(),bt));
+        figury.add(new Prostokat(scanner.nextDouble(),scanner.nextDouble()));
 
         for(Figura wys : figury){
             System.out.println(wys.getClass().getSimpleName() +": obwod: " + wys.obwod() + " powierzchnia " + wys.powierzchnia() );
